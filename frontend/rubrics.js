c7ae53e1112772c9088d9248ee9d44cfe6a1ed71
morozov1982 <morozov1982@gmail.com>
2025-02-17 17:20:35 +0500

    lesson_43 files final

--- frontend/rubrics.js
+++ frontend/rubrics.js
@@ -16,6 +16,15 @@ async function loadItem(evt) {
         console.log(result.statusText);
 }
 
+async function deleteItem(evt) {
+    evt.preventDefault();
+    const result = await fetch(evt.target.href, { method: 'DELETE' });
+    if (result.ok)
+        loadList();
+    else
+        console.log(result.statusText);
+}
+
 async function loadList() {
     const result = await fetch(`${domain}rubrics`);
 
@@ -26,7 +35,9 @@ async function loadList() {
         for (let i = 0; i < data.length; i++) {
             d = data[i];
             s += `<li>${d.name} 
-                      <a href="${domain}rubrics/${d.id}/" class="detail">Вывести</a></li>`;
+                      <a href="${domain}rubrics/${d.id}/" class="detail">Вывести</a>
+                      <a href="${domain}rubrics/${d.id}/" class="delete">Удалить</a>
+                  </li>`;
         }
 
         list.innerHTML = s;
@@ -36,9 +47,38 @@ async function loadList() {
             link.addEventListener('click', loadItem);
         });
 
+        links = list.querySelectorAll('li a.delete');
+        links.forEach((link) => {
+            link.addEventListener('click', deleteItem);
+        });
+
     } else 
         // window.alert(result.statusText);
         console.log(result.statusText);
 }
 
 loadList();
+
+itemName.form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    let url, method;
+    if (itemId.value) {
+        url = `${domain}rubrics/${itemId.value}/`;
+        method = 'PUT';
+    } else {
+        url = `${domain}rubrics/`;
+        method = 'POST';
+    }
+
+    const result = await fetch(url, {
+        method: method,
+        body: JSON.stringify({ name: itemName.value }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (result.ok) {
+        loadList();
+        itemName.form.reset();
+        itemId.value = '';
+    } else
+        console.log(result.statusText);
+});
