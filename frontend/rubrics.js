931014747d71d2270c2e7206cccfa9b6a1b3771c
morozov1982 <morozov1982@gmail.com>
2025-02-17 15:46:15 +0500

    lesson_42 final files

--- frontend/rubrics.js
+++ frontend/rubrics.js
@@ -1,9 +1,20 @@
-console.log('Работает');
-
 const domain = 'http://localhost:8000/api/';
 
 // const list = document.getElementById('list');
 const list = document.querySelector('#list');
+const itemId = document.querySelector('#id');
+const itemName = document.querySelector('#name');
+
+async function loadItem(evt) {
+    evt.preventDefault();
+    const result = await fetch(evt.target.href);
+    if (result.ok) {
+        const data = await result.json();
+        itemId.value = data.id;
+        itemName.value = data.name;
+    } else
+        console.log(result.statusText);
+}
 
 async function loadList() {
     const result = await fetch(`${domain}rubrics`);
@@ -14,10 +25,17 @@ async function loadList() {
         
         for (let i = 0; i < data.length; i++) {
             d = data[i];
-            s += `<li>${d.name}</li>`;
+            s += `<li>${d.name} 
+                      <a href="${domain}rubrics/${d.id}/" class="detail">Вывести</a></li>`;
         }
 
         list.innerHTML = s;
+
+        let links = list.querySelectorAll('li a.detail');
+        links.forEach((link) => {
+            link.addEventListener('click', loadItem);
+        });
+
     } else 
         // window.alert(result.statusText);
         console.log(result.statusText);
